966fd6f3bc365eb5ce2504114d90707b4104d08e
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-05-30 07:25:04 +0600

    [DONE] Create User Service & Route & controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 // cors
@@ -9,9 +10,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//
+app.use('/api/v1/users/', usersRouter)
 // Testing
-app.get('/', (req: Request, res: Response): void => {
-  res.send('Working Successfully')
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working successfully')
 })
 
 export default app
---
+++ src/app/modules/users/users.utils.ts
@@ -0,0 +1,21 @@
+// find the last IdNumber from  database
+
+import { User } from './users.model'
+
+export const findLastUserId = async () => {
+  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({ createdAt: -1 })
+    .lean()
+
+  return lastUser?.id
+}
+
+// id generator function
+
+export const generateUserId = async () => {
+  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
+
+  // incremnet by 1
+  const incrementedID = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return incrementedID
+}
